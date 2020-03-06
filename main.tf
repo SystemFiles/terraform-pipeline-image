ecfa57e5f1bde454a010a568126ae4c9b5642ad1
Ben Sykes <ben@sykesdev.ca>
2020-03-05 22:05:23 -0500

    update features block

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-    version = 2.0
-    features = []
+    # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+    version = "=2.0.0"
+    features {}
 }
-
 
 resource "azurerm_resource_group" "rg" {
     name     = "bensykes-dev-rg"
