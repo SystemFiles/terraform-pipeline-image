52e79b3b94c7832958f6016e2a5cc219684da989
Ben Sykes <bensykes12@gmail.com>
2020-03-14 19:26:52 -0400

    ...

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Login to azure using Service principle (Terraform AD Service Principal in use)
+echo "Logging into azure through Terraform AD SP"
 az login --service-principal -u http://Terraform -p $pass --tenant $tenant
 
 # Initialize and validate Terraform (*.tf)
