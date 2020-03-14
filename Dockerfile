b14c22dfa157023798d416b84fd7d40f536cf3a8
Ben Sykes <bensykes12@gmail.com>
2020-03-14 19:57:47 -0400

    ...

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,19 @@ ARG tenant
 ARG tf_version
 
 FROM microsoft/azure-cli:latest as az
-
 WORKDIR /terraform
 
+# Set appropriate environment varaibles
+ENV AZ_PASS=$pass
+ENV AZ_TENANT=$tenant
+ENV tf_version=$tf_version
 COPY . .
 
 # Login to azure
 RUN apk add --update --no-cache git bash openssl
 
 # Install git and terraform
-RUN wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip && \
+RUN wget https://releases.hashicorp.com/terraform/${tf_version}/terraform_${tf_version}_linux_amd64.zip && \
     unzip terraform_0.12.19_linux_amd64.zip && \
     rm terraform_0.12.19_linux_amd64.zip && \
     mv terraform /usr/local/bin
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 # Login to azure using Service principle (Terraform AD Service Principal in use)
 echo "Logging into azure through Terraform AD SP"
-echo $pass
-echo $tenant
-az login --service-principal -u http://Terraform -p 8ef52391-bdd6-4b34-8ea6-1b11a8ce7da7 --tenant 0c4c6c9a-7ce2-4aa8-ab35-7a74ad4c80c3
-# az login --service-principal --tenant $tenant -u http://Terraform -p $pass
+echo $AZ_PASS
+echo $AZ_TENANT
+# az login --service-principal -u http://Terraform -p 8ef52391-bdd6-4b34-8ea6-1b11a8ce7da7 --tenant 0c4c6c9a-7ce2-4aa8-ab35-7a74ad4c80c3
+az login --service-principal --tenant $AZ_TENANT -u http://Terraform -p $AZ_PASS
 
 # Initialize and validate Terraform (*.tf)
 terraform init
