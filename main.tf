01a3f142f303760930ac1c74e8239b13e4a7d605
Ben Sykes <bensykes12@gmail.com>
2020-03-14 21:40:27 -0400

    remove provider

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider "azurerm" {
-    # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-    version = "=1.43.0"
-    features {}
-}
-
 resource "azurerm_resource_group" "rg" {
     name     = "bensykes-dev-rg"
     location = "canadacentral"
