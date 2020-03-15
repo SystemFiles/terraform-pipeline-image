39ecef0cc139d3c78b827be7524ab2642366303e
Ben Sykes <bensykes12@gmail.com>
2020-03-15 07:49:54 -0400

    try manual login

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 
 # Login to azure using Service principle (Terraform AD Service Principal in use)
 echo "Logging into azure through Terraform AD SP..."
-# az login --service-principal -u http://Terraform -p 8ef52391-bdd6-4b34-8ea6-1b11a8ce7da7 --tenant 0c4c6c9a-7ce2-4aa8-ab35-7a74ad4c80c3
-az login --service-principal -u $AZ_APP_ID -p $AZ_PASS --tenant $AZ_TENANT
+# az login --service-principal -u $AZ_APP_ID -p $AZ_PASS --tenant $AZ_TENANT
+az login
 echo "Done."
 # Initialize and validate Terraform (*.tf)
 echo "Initializing terraform and validating code for syntax errors..."
