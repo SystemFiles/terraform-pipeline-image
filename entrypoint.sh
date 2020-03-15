4114071a1388df293e88d1baab4298ac8a3e2d64
Ben Sykes <bensykes12@gmail.com>
2020-03-15 08:07:45 -0400

    FIXED AUTH FINALLY I AM A GENIUS

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 
 echo "Setting environment variables for Terraform"
 export ARM_SUBSCRIPTION_ID="3a2efedf-fc87-4c5e-b3f8-a089ad33a2e8"
-export ARM_CLIENT_ID="89e8ca16-1090-49e0-a4c9-d8927b2835aa"
-export ARM_CLIENT_SECRET="2e10ee4d-3034-44d5-9a88-2e8df2a9d807"
-export ARM_TENANT_ID="0c4c6c9a-7ce2-4aa8-ab35-7a74ad4c80c3"
+export ARM_CLIENT_ID="$AZ_APP_ID"
+export ARM_CLIENT_SECRET="$AZ_PASS"
+export ARM_TENANT_ID="$AZ_TENANT"
 
 # Not needed for public, required for usgovernment, german, china
 export ARM_ENVIRONMENT=public
