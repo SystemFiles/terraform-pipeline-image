e18a3ff18d3ec28402f49af88f3d4315f8a2b9ae
Ben Sykes <bensykes12@gmail.com>
2020-03-14 21:54:45 -0400

    updated azurerm provider

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "azurerm" {
+    # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+    version = "=2.1.0"
+    features {}
+}
+
 resource "azurerm_resource_group" "rg" {
     name     = "bensykes-dev-rg"
     location = "canadacentral"
