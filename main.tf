20d1f3bbf330c47a8818c4555eea9ce44537f363
BuildTools <unconfigured@null.spigotmc.org>
2020-09-16 07:59:03 -0400

    build workstation

--- main.tf
+++ main.tf
@@ -4,19 +4,72 @@ provider "azurerm" {
     features {}
 }
 
+variable "prefix" {
+	default = "bsykes
+}
+
 resource "azurerm_resource_group" "rg" {
-    name     = "bensykes-dev-rg"
+    name     = "bensykes-workstations-rg"
     location = "canadacentral"
 }
 
-resource "azurerm_storage_account" "sa" {
-    name                     = "storeacctwo"
-    resource_group_name      = azurerm_resource_group.rg.name
-    location                 = azurerm_resource_group.rg.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-    tags = {
-        environment = "dev"
-    }
+resource "azurerm_subnet" "subnet" {
+  name                 = "workstationssubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "${var.prefix}CCSubnet"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "bensykes-cc-win"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "${var.prefix}osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.prefix}"
+    admin_username = "ben.sykes"
+    admin_password = "defaultPasw04d$$"
+  }
+  tags = {
+    environment = "staging"
+  }
 }
